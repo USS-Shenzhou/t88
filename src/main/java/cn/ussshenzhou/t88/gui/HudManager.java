924fb5641be880be47d5a05c31a347ae41c0213b
USS-Shenzhou <969840795@qq.com>
2023-01-31 20:27:03 +0800

    add HUD support to TComponent

---
+++ src/main/java/cn/ussshenzhou/t88/gui/HudManager.java
@@ -0,0 +1,70 @@
+package cn.ussshenzhou.t88.gui;
+
+import cn.ussshenzhou.t88.gui.event.ResizeHudEvent;
+import cn.ussshenzhou.t88.gui.util.MouseHelper;
+import cn.ussshenzhou.t88.gui.widegt.TComponent;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.LinkedHashSet;
+
+/**
+ * @author USS_Shenzhou
+ * TODO move to T88
+ */
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class HudManager {
+    private static final LinkedHashSet<TComponent> CHILDREN = new LinkedHashSet<>();
+
+    public static void addC(TComponent tComponent) {
+        CHILDREN.add(tComponent);
+    }
+
+    public static void remove(TComponent tComponent) {
+        CHILDREN.remove(tComponent);
+    }
+
+    @SubscribeEvent
+    public static void onRenderHud(RenderGameOverlayEvent event) {
+        if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) {
+            return;
+        }
+        PoseStack poseStack = event.getMatrixStack();
+        int mouseX = (int) MouseHelper.getMouseX();
+        int mouseY = (int) MouseHelper.getMouseY();
+        float partialTick = event.getPartialTicks();
+        CHILDREN.forEach((tComponent) -> {
+            if (tComponent.isVisibleT()) {
+                tComponent.render(poseStack, mouseX, mouseY, partialTick);
+            }
+        });
+        CHILDREN.forEach((tComponent) -> {
+            if (tComponent.isVisibleT()) {
+                tComponent.renderTop(poseStack, mouseX, mouseY, partialTick);
+            }
+        });
+    }
+
+    @SubscribeEvent
+    public static void onTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            CHILDREN.forEach((tComponent) -> {
+                if (tComponent.isVisibleT()) {
+                    tComponent.tickT();
+                }
+            });
+        }
+    }
+
+    @SubscribeEvent
+    public static void onResize(ResizeHudEvent event) {
+        int screenWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
+        int screenHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
+        CHILDREN.forEach(tComponent -> tComponent.resizeAsHud(screenWidth, screenHeight));
+    }
+}
---
+++ src/main/java/cn/ussshenzhou/t88/mixin/MinecraftMixin.java
@@ -0,0 +1,21 @@
+package cn.ussshenzhou.t88.mixin;
+
+import cn.ussshenzhou.t88.gui.event.ResizeHudEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * @author USS_Shenzhou
+ */
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+
+    @Inject(method = "resizeDisplay",at = @At("RETURN"))
+    private void resizeHudT88(CallbackInfo ci){
+        MinecraftForge.EVENT_BUS.post(new ResizeHudEvent());
+    }
+}
