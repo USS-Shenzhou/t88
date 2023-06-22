1c51c4609b5e32ec2aa3004b7f25369a0ebea4c3
USS-Shenzhou <969840795@qq.com>
2023-06-22 12:15:31 +0800

    tooltip test

--- src/main/java/cn/ussshenzhou/t88/mixin/AbstractWidgetMixin.java
+++ src/main/java/cn/ussshenzhou/t88/mixin/AbstractWidgetMixin.java
@@ -1,6 +1,8 @@
 package cn.ussshenzhou.t88.mixin;
 
 import cn.ussshenzhou.t88.gui.widegt.TWidget;
+import net.minecraft.client.InputType;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,9 +22,18 @@ public abstract class AbstractWidgetMixin {
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/AbstractWidget;getY()I"))
     private int t88TScrollPanelCompatability(AbstractWidget instance) {
         if (instance instanceof TWidget tWidget) {
-            return (int) (getY() + tWidget.getParentScrollAmountIfExist());
+            return (int) (getY() - tWidget.getParentScrollAmountIfExist());
         } else {
             return getY();
+        }
+    }
+
+    @Redirect(method = "updateTooltip",at = @At(value = "INVOKE",target = "Lnet/minecraft/client/InputType;isKeyboard()Z"))
+    private boolean t88IgnoreTooltipRequireLastInput(InputType instance){
+        if (this instanceof TWidget){
+            return true;
+        } else {
+            return Minecraft.getInstance().getLastInputType().isKeyboard();
         }
     }
 }
