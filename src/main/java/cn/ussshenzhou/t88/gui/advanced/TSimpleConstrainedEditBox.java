e5b8556b58da42a27d0077fd1ea71c43fa728bf9
USS-Shenzhou <969840795@qq.com>
2023-07-25 22:10:01 +0800

    minor fixes

--- src/main/java/cn/ussshenzhou/t88/gui/advanced/TSimpleConstrainedEditBox.java
+++ src/main/java/cn/ussshenzhou/t88/gui/advanced/TSimpleConstrainedEditBox.java
@@ -10,17 +10,23 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
  */
 public class TSimpleConstrainedEditBox extends TConstrainedEditBox {
     private ArgumentType<?> argument;
+    private boolean justSimpleText = false;
 
     public TSimpleConstrainedEditBox(ArgumentType<?> argument) {
         super();
         this.argument = argument;
     }
 
+    public TSimpleConstrainedEditBox(ArgumentType<?> argument, boolean justSimpleText) {
+        this.argument = argument;
+        this.justSimpleText = justSimpleText;
+    }
+
     @Override
     public void checkAndThrow(String value) throws CommandSyntaxException {
         StringReader stringReader = new StringReader(value);
         argument.parse(stringReader);
-        if (stringReader.peek() != CommandDispatcher.ARGUMENT_SEPARATOR_CHAR) {
+        if (!justSimpleText && stringReader.peek() != CommandDispatcher.ARGUMENT_SEPARATOR_CHAR) {
             throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherExpectedArgumentSeparator().createWithContext(stringReader);
         }
     }
