994b3d570c72e85306414941e7188b13530866cf
USS-Shenzhou <969840795@qq.com>
2024-01-20 20:09:57 +0800

    optibad

--- src/main/java/cn/ussshenzhou/t88/mixin/ChunkRenderDispatcher$RenderChunk$RebuildTaskMixin.java
+++ src/main/java/cn/ussshenzhou/t88/mixin/ChunkRenderDispatcher$RenderChunk$RebuildTaskMixin.java
@@ -2,7 +2,6 @@ package cn.ussshenzhou.t88.mixin;
 
 import cn.ussshenzhou.t88.render.ChunkCompileContext;
 import cn.ussshenzhou.t88.render.IFixedModelBlockEntity;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexFormat;
@@ -16,12 +15,16 @@ import net.minecraft.client.renderer.chunk.VisGraph;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.ModelData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Surrogate;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -34,42 +37,74 @@ import java.util.Set;
 @Mixin(net.minecraft.client.renderer.chunk.ChunkRenderDispatcher.RenderChunk.RebuildTask.class)
 public class ChunkRenderDispatcher$RenderChunk$RebuildTaskMixin {
 
-    @Inject(method = "compile", at = @At(value = "INVOKE", shift = At.Shift.AFTER,
-            target = "Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask;handleBlockEntity(Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask$CompileResults;Lnet/minecraft/world/level/block/entity/BlockEntity;)V"))
+
+    @SuppressWarnings("MissingUnique")
+    //needtest
+    @Surrogate
     private void t88CompileFixedBlockEntity(float pX, float pY, float pZ, ChunkBufferBuilderPack pChunkBufferBuilderPack,
                                             CallbackInfoReturnable<ChunkRenderDispatcher.RenderChunk.RebuildTask.CompileResults> cir,
-                                            @Local RenderChunkRegion renderchunkregion, @Local PoseStack poseStack, @Local Set<RenderType> renderTypes,
-                                            @Local RandomSource random, @Local BlockRenderDispatcher blockDispatcher, @Local(ordinal = 2) BlockPos pos,
-                                            @Local BlockState state, @Local BlockEntity entity) {
+                                            ChunkRenderDispatcher.RenderChunk.RebuildTask.CompileResults compileResults, int i, BlockPos from, BlockPos to, VisGraph visgraph,
+                                            RenderChunkRegion renderchunkregion, PoseStack poseStack,
+                                            Set<RenderType> renderTypes, RandomSource random,
+                                            BlockRenderDispatcher blockDispatcher, Iterator<BlockPos> posIterator,
+                                            BlockPos pos,
+                                            BlockState state, BlockEntity entity) {
         if (entity instanceof IFixedModelBlockEntity fixedModelBlockEntity) {
-            var context = fixedModelBlockEntity.handleCompileContext(new ChunkCompileContext(renderchunkregion, poseStack, blockDispatcher, pos, state, entity));
-            if (context == null) {
-                return;
+            t88compileFixedBlockEntityInternal(pChunkBufferBuilderPack, poseStack, renderTypes, random, blockDispatcher, state, entity, fixedModelBlockEntity, renderchunkregion, pos);
+        }
+    }
+
+    /**
+     * O p t i b a d.
+     */
+    @SuppressWarnings({"AlibabaLowerCamelCaseVariableNaming", "InvalidInjectorMethodSignature"})
+    @Inject(method = "compile", at = @At(value = "INVOKE", shift = At.Shift.AFTER,
+            target = "Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask;handleBlockEntity(Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask$CompileResults;Lnet/minecraft/world/level/block/entity/BlockEntity;)V"),
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void t88CompileFixedBlockEntity(float pX, float pY, float pZ, ChunkBufferBuilderPack pChunkBufferBuilderPack,
+                                            CallbackInfoReturnable<ChunkRenderDispatcher.RenderChunk.RebuildTask.CompileResults> cir,
+                                            ChunkRenderDispatcher.RenderChunk.RebuildTask.CompileResults compileResults, int i, BlockPos from, BlockPos to, VisGraph visgraph,
+                                            PoseStack poseStack, @Coerce Object chunkCacheOF, @Coerce Object singleIterableOF, boolean wtf0OF, boolean wtf1OF,
+                                            Set<RenderType> renderTypes, RandomSource random,
+                                            BlockRenderDispatcher blockDispatcher, Iterator<BlockPos> posIterator,
+                                            @Coerce Object posOF,
+                                            BlockState state, BlockEntity entity) {
+        if (entity instanceof IFixedModelBlockEntity fixedModelBlockEntity) {
+            var renderChunkRegion = (BlockAndTintGetter) chunkCacheOF;
+            var pos = (BlockPos) posOF;
+            t88compileFixedBlockEntityInternal(pChunkBufferBuilderPack, poseStack, renderTypes, random, blockDispatcher, state, entity, fixedModelBlockEntity, renderChunkRegion, pos);
+        }
+    }
+
+    @Unique
+    private void t88compileFixedBlockEntityInternal(ChunkBufferBuilderPack pChunkBufferBuilderPack, PoseStack poseStack, Set<RenderType> renderTypes, RandomSource random, BlockRenderDispatcher blockDispatcher, BlockState state, BlockEntity entity, IFixedModelBlockEntity fixedModelBlockEntity, BlockAndTintGetter renderChunkRegion, BlockPos pos) {
+        var context = fixedModelBlockEntity.handleCompileContext(new ChunkCompileContext(renderChunkRegion, poseStack, blockDispatcher, pos, state, entity));
+        if (context == null) {
+            return;
+        }
+        var renderType = context.renderType;
+        var builder = pChunkBufferBuilderPack.builder(renderType);
+        if (renderTypes.add(renderType)) {
+            builder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.BLOCK);
+        }
+        if (context.bakedModel != null) {
+            poseStack.pushPose();
+            context.beforeBakedModel.accept(poseStack);
+            var model = context.bakedModel;
+            if (model != null) {
+                blockDispatcher.getModelRenderer().tesselateBlock(renderChunkRegion, model,
+                        context.bakedModelBlockState == null ? state : context.bakedModelBlockState,
+                        pos, poseStack, builder, true, random, state.getSeed(pos), OverlayTexture.NO_OVERLAY,
+                        model.getModelData(renderChunkRegion, pos, state, ((ChunkRenderDispatcher.RenderChunk.RebuildTask) (Object) this).getModelData(pos)),
+                        renderType);
             }
-            var renderType = context.renderType;
-            var builder = pChunkBufferBuilderPack.builder(renderType);
-            if (renderTypes.add(renderType)) {
-                builder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.BLOCK);
-            }
-            if (context.bakedModel != null) {
-                poseStack.pushPose();
-                context.beforeBakedModel.accept(poseStack);
-                var model = context.bakedModel;
-                if (model != null) {
-                    blockDispatcher.getModelRenderer().tesselateBlock(renderchunkregion, model,
-                            context.bakedModelBlockState == null ? state : context.bakedModelBlockState,
-                            pos, poseStack, builder, true, random, state.getSeed(pos), OverlayTexture.NO_OVERLAY,
-                            model.getModelData(renderchunkregion, pos, state, ((ChunkRenderDispatcher.RenderChunk.RebuildTask) (Object) this).getModelData(pos)),
-                            renderType);
-                }
-                poseStack.popPose();
-            }
-            if (context.needRenderAdditional) {
-                poseStack.pushPose();
-                poseStack.translate(pos.getX(), pos.getY(), pos.getZ());
-                fixedModelBlockEntity.renderAdditional(context, renderTypes, pChunkBufferBuilderPack, poseStack, OverlayTexture.NO_OVERLAY);
-                poseStack.popPose();
-            }
+            poseStack.popPose();
+        }
+        if (context.needRenderAdditional) {
+            poseStack.pushPose();
+            poseStack.translate(pos.getX(), pos.getY(), pos.getZ());
+            fixedModelBlockEntity.renderAdditional(context, renderTypes, pChunkBufferBuilderPack, poseStack, OverlayTexture.NO_OVERLAY);
+            poseStack.popPose();
         }
     }
 }
