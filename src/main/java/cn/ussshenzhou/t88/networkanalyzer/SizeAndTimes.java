49451457598507c911e6c82f58dfbae85d54445c
USS-Shenzhou <969840795@qq.com>
2024-09-22 11:32:43 +0800

    bug fix

--- src/main/java/cn/ussshenzhou/t88/networkanalyzer/SizeAndTimes.java
+++ src/main/java/cn/ussshenzhou/t88/networkanalyzer/SizeAndTimes.java
@@ -4,23 +4,27 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author USS_Shenzhou
  */
 public class SizeAndTimes {
-    private int size, times;
+    private final AtomicInteger size = new AtomicInteger(0);
+    private final AtomicInteger times = new AtomicInteger(0);
 
     public static final StreamCodec<ByteBuf, SizeAndTimes> STREAM_CODEC = StreamCodec.composite(
             ByteBufCodecs.VAR_INT,
-            s -> s.size,
+            s -> s.size.get(),
             ByteBufCodecs.VAR_INT,
-            s -> s.times,
+            s -> s.times.get(),
             SizeAndTimes::new
     );
 
+
     private SizeAndTimes(int size, int times) {
-        this.size = size;
-        this.times = times;
+        this.size.set(size);
+        this.times.set(times);
     }
 
     public SizeAndTimes(int size) {
@@ -29,16 +33,16 @@ public class SizeAndTimes {
     }
 
     public int getSize() {
-        return size;
+        return size.get();
     }
 
     public int getTimes() {
-        return times;
+        return times.get();
     }
 
     public SizeAndTimes increaseSize(int size) {
-        this.size += size;
-        times++;
+        this.size.addAndGet(size);
+        times.incrementAndGet();
         return this;
     }
 }
