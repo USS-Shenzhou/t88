44a3973954c0c4355e287a80cbc411f18c0dc1fb
nutant <65939836+nutant233@users.noreply.github.com>
2026-04-02 15:39:18 +0800

    add an onLoad method to be called when the configuration is loaded.

--- src/main/java/cn/ussshenzhou/t88/config/ConfigHelper.java
+++ src/main/java/cn/ussshenzhou/t88/config/ConfigHelper.java
@@ -66,6 +66,7 @@ public class ConfigHelper {
             } else {
                 FileUtils.write(configFile, GSON.toJson(newInstance), StandardCharsets.UTF_8);
             }
+            newInstance.onLoad();
             CACHE.put(newInstance.getClass(), newInstance);
             saveConfigInternal(newInstance, configFile);
         } catch (IOException ignored) {
